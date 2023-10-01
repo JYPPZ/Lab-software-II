93b24d2f8cfc42599f82b73d7d397ce7a16a7c6f
JYPPZ <juanesyepezr@gmail.com>
2023-10-01 16:26:40 -0500

    separacion de responsabilidades en clase Product repository

---
+++ CrudProducts/src/main/java/co/edu/unicauca/openmarket/access/ConnectRepository.java
@@ -0,0 +1,63 @@
+package co.edu.unicauca.openmarket.access;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConnectRepository {
+    private Connection conn;
+    public ConnectRepository(){
+        initDatabase();
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    private void initDatabase() {
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS products (\n"
+                + "	productId integer PRIMARY KEY AUTOINCREMENT,\n"
+                + "	name text NOT NULL,\n"
+                + "	description text NULL\n"
+                + ");";
+
+        try {
+            this.connect();
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+            //this.disconnect();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void connect() {
+        // SQLite connection string
+        //String url = "jdbc:sqlite:./myDatabase.db"; //Para Linux/Mac
+        //String url = "jdbc:sqlite:C:/sqlite/db/myDatabase.db"; //Para Windows
+        String url = "jdbc:sqlite::memory:";
+
+        try {
+            conn = DriverManager.getConnection(url);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+}
