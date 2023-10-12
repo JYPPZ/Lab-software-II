3243366cf0104ee6faa5b9a3da8668251874ae69
JYPPZ <juanesyepezr@gmail.com>
2023-10-12 00:06:37 -0500

    se agregaron sugerencias de codigo

--- CrudProducts/src/main/java/co/edu/unicauca/openmarket/access/CategoryRepository.java
+++ CrudProducts/src/main/java/co/edu/unicauca/openmarket/access/CategoryRepository.java
@@ -6,12 +6,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CategoryRepository implements ICategoryRepository{
-    private Connection conn;
+    private final Connection conn;
 
     public CategoryRepository(Connection conn) {
         this.conn = conn;
--- CrudProducts/src/main/java/co/edu/unicauca/openmarket/access/ICategoryRepository.java
+++ CrudProducts/src/main/java/co/edu/unicauca/openmarket/access/ICategoryRepository.java
@@ -1,8 +1,6 @@
 package co.edu.unicauca.openmarket.access;
 import co.edu.unicauca.openmarket.domain.Category;
 
-import java.util.List;
-
 public interface ICategoryRepository {
     boolean save(Category miCategoria);
 
